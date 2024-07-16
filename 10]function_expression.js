7b709e6c985fdaed386f8a0a59c57eeb4519a01f
siddhiamale <amalesiddhi893@gmail.com>
2024-07-16 13:18:31 +0530

    update notes

---
+++ 10]function_expression.js
@@ -0,0 +1,10 @@
+// Function expressions are the nameless function. It is another way to define function
+
+// syntax :-
+// const variable = function (arg1,arg2...) {
+//     //do or return something
+// }
+
+const sum = function(a,b) {
+    return a+b;
+}
